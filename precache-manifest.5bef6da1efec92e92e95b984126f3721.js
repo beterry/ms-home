68879335e1c3812f717627916c42dd8001d34da4
Ben Terry <beterry12@gmail.com>
2020-08-12 12:52:12 -0400

    Updates

---
+++ precache-manifest.5bef6da1efec92e92e95b984126f3721.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38624d91399c308c9943a00830c53124",
+    "url": "/github.io/ms-home/index.html"
+  },
+  {
+    "revision": "9f8a31928bc59a9ae044",
+    "url": "/github.io/ms-home/static/css/main.d1801073.chunk.css"
+  },
+  {
+    "revision": "37636bbba59be98fbf7a",
+    "url": "/github.io/ms-home/static/js/2.b8ac7734.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github.io/ms-home/static/js/2.b8ac7734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f8a31928bc59a9ae044",
+    "url": "/github.io/ms-home/static/js/main.d7001028.chunk.js"
+  },
+  {
+    "revision": "de1551a354daadf59b92",
+    "url": "/github.io/ms-home/static/js/runtime-main.e4036d91.js"
+  },
+  {
+    "revision": "72b61a83931a45924497ae7d8aa4273a",
+    "url": "/github.io/ms-home/static/media/anthony-sottasante@2x.72b61a83.jpg"
+  },
+  {
+    "revision": "7d77a0d930724b1b2d44acec2e5fb028",
+    "url": "/github.io/ms-home/static/media/bill-brown@2x.7d77a0d9.jpg"
+  },
+  {
+    "revision": "75455e395195c57c00acb15d9c83b8e7",
+    "url": "/github.io/ms-home/static/media/fb.75455e39.svg"
+  },
+  {
+    "revision": "9b02f8e7314a411f83cff302e01e8170",
+    "url": "/github.io/ms-home/static/media/icon-design.9b02f8e7.svg"
+  },
+  {
+    "revision": "197db1e8e70ea4e73c8ae2637d29a68b",
+    "url": "/github.io/ms-home/static/media/icon-money.197db1e8.svg"
+  },
+  {
+    "revision": "ea043015212afe01cf8a2cb7e24ab2ed",
+    "url": "/github.io/ms-home/static/media/icon-quote.ea043015.svg"
+  },
+  {
+    "revision": "689d250475dd47114759c1933c2a15b1",
+    "url": "/github.io/ms-home/static/media/icon-week.689d2504.svg"
+  },
+  {
+    "revision": "cd74d5b0bea59cf16f5d766dd62b1c3e",
+    "url": "/github.io/ms-home/static/media/insta.cd74d5b0.svg"
+  },
+  {
+    "revision": "8a82317bd6fb02fcc689fba864943c42",
+    "url": "/github.io/ms-home/static/media/logo.8a82317b.svg"
+  },
+  {
+    "revision": "3a2f40d13dd06c75216946c08ca8677e",
+    "url": "/github.io/ms-home/static/media/marcos.3a2f40d1.svg"
+  },
+  {
+    "revision": "e82ea36a9b3e8091ef3d93e569b261d4",
+    "url": "/github.io/ms-home/static/media/meineke.e82ea36a.svg"
+  },
+  {
+    "revision": "c715626a40314f61426b9b113903878a",
+    "url": "/github.io/ms-home/static/media/midas.c715626a.svg"
+  },
+  {
+    "revision": "2216822e1e05e419c70de46942c096ab",
+    "url": "/github.io/ms-home/static/media/mikes.2216822e.svg"
+  },
+  {
+    "revision": "55d448fad7d21363c316aa0d8fb7b17e",
+    "url": "/github.io/ms-home/static/media/minus.55d448fa.svg"
+  },
+  {
+    "revision": "036649107d8c7d1f7b32d14dbb302c01",
+    "url": "/github.io/ms-home/static/media/papa.03664910.svg"
+  },
+  {
+    "revision": "f2dddc934c7b20c05d7d78bf009494e0",
+    "url": "/github.io/ms-home/static/media/plus.f2dddc93.svg"
+  },
+  {
+    "revision": "a161aebb725d07ed3bb0e1350175549c",
+    "url": "/github.io/ms-home/static/media/product-brochures.a161aebb.jpg"
+  },
+  {
+    "revision": "0cd1699080f38baf0a93bdddd5cf9fe5",
+    "url": "/github.io/ms-home/static/media/product-direct-mail-letters.0cd16990.jpg"
+  },
+  {
+    "revision": "a7ecee0626a214c1a85a084936db33b4",
+    "url": "/github.io/ms-home/static/media/product-door-hangers.a7ecee06.jpg"
+  },
+  {
+    "revision": "0a20434767aa53b4432f6e0ff8ad86d1",
+    "url": "/github.io/ms-home/static/media/product-menus.0a204347.jpg"
+  },
+  {
+    "revision": "69920c141d8445ca1d1b8d26c7334067",
+    "url": "/github.io/ms-home/static/media/product-plastic-postcards.69920c14.jpg"
+  },
+  {
+    "revision": "1c6ce1451b95b1872f7e6a6b4f7e1113",
+    "url": "/github.io/ms-home/static/media/product-postcard-magnets.1c6ce145.jpg"
+  },
+  {
+    "revision": "1640624b367dd2e746f66c60418b3488",
+    "url": "/github.io/ms-home/static/media/product-postcards.1640624b.jpg"
+  },
+  {
+    "revision": "454223c5d4028e4bb006f673c9e73d87",
+    "url": "/github.io/ms-home/static/media/product-scratch-off-postcards.454223c5.jpg"
+  },
+  {
+    "revision": "ecf6b20dab861fae09add44e21367ab0",
+    "url": "/github.io/ms-home/static/media/resource-1.ecf6b20d.jpg"
+  },
+  {
+    "revision": "084efdd4a2499b597a09694c3b3b6a1c",
+    "url": "/github.io/ms-home/static/media/resource-2.084efdd4.jpg"
+  },
+  {
+    "revision": "587a94e0cf41eed6ae1ef34ca04dbc58",
+    "url": "/github.io/ms-home/static/media/resource-3.587a94e0.jpg"
+  },
+  {
+    "revision": "b65346a92da0f68a7c786bc2a1f424b1",
+    "url": "/github.io/ms-home/static/media/resource-4.b65346a9.jpg"
+  },
+  {
+    "revision": "05b65fddf277f7c745a272f0abcc8985",
+    "url": "/github.io/ms-home/static/media/shark.05b65fdd.svg"
+  },
+  {
+    "revision": "17bb3a5fd18a52d8c4b61952adbc0487",
+    "url": "/github.io/ms-home/static/media/terry-mckim@2x.17bb3a5f.jpg"
+  },
+  {
+    "revision": "ba188d6d3d57471a16bccddd67268bc3",
+    "url": "/github.io/ms-home/static/media/wave-sm.ba188d6d.svg"
+  },
+  {
+    "revision": "373f37a22ef2763ffafee2beafb7773d",
+    "url": "/github.io/ms-home/static/media/wave.373f37a2.svg"
+  }
+]);
